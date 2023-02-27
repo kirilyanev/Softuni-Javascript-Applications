fa27bc432e5efcb7f83a12fd21f1f4b2e4689564
Kiril Yanev <kl_yanev@abv.bg>
2023-02-27 17:30:26 +0200

    Solution for 02.Github-Repos exercise

--- 05-Asynchronous-Programming/02.Github-Repos/scripts/app.js
+++ 05-Asynchronous-Programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,31 @@
 function loadRepos() {
-	console.log("TODO...");
+	const username = document.getElementById('username').value;
+	const output = document.getElementById('repos');
+
+	const url = `https://api.github.com/users/${username}/repos`;
+	fetch(url).then(res => {
+		console.log(res);
+			if(res.ok == false) {
+				console.log('Error encountered:',res.status,res.statusText);
+				// return Promise.reject(`${res.status} ${res.statusText}`);
+				throw `${res.status} ${res.statusText}`
+			}
+			return res.json();
+		})
+		.then(data => {
+			console.log(data);
+			output.replaceChildren();
+			data.forEach(repo => {
+				let liElement = document.createElement('li');
+				let aElement = document.createElement('a');
+				aElement.textContent = repo.full_name;
+				aElement.setAttribute("href", `${repo.html_url}`);
+				liElement.appendChild(aElement);
+				output.appendChild(liElement);
+			});
+		})
+		.catch (err => {
+			output.innerHTML = `<p>Error encountered: ${err}</p>`;
+
+		})
 }
