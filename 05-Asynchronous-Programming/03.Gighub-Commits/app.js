bafc77590b3a2a59d66203ee81cffdb24b64496c
Kiril Yanev <kl_yanev@abv.bg>
2023-02-27 17:55:56 +0200

    03.Github-Commits solution

--- 05-Asynchronous-Programming/03.Gighub-Commits/app.js
+++ 05-Asynchronous-Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,26 @@
 function loadCommits() {
     // Try it with Fetch API
-    console.log('TODO...');
+    const username = document.getElementById('username').value;
+    const repository = document.getElementById('repo').value;
+    const commits = document.getElementById('commits');
+    const url = `https://api.github.com/repos/${username}/${repository}/commits`;
+
+    fetch(url).then(response => {
+        if(response.ok == false) {
+            throw `Error: ${response.status} (Not Found)`;
+        }
+        return response.json();
+    }).then(data => {
+        commits.replaceChildren();
+        for (let entry of data) {
+            const liElement = document.createElement('li');
+            liElement.textContent = `${entry.commit.author.name}: ${entry.commit.message}`;
+            commits.appendChild(liElement);
+        }
+    }).catch(error => {
+        commits.replaceChildren();
+        const liElement = document.createElement('li');
+        liElement.textContent = `${error}`;
+        commits.appendChild(liElement);
+    })
 }
