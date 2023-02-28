8a091ea2611af428fe0b7019d603b12b17a1bfbb
Kiril Yanev <kl_yanev@abv.bg>
2023-02-28 13:12:36 +0200

    02-Bus-Schedule solution

--- 06-Exercise-Asynchronous-Programming/02.Bus-Schedule/app.js
+++ 06-Exercise-Asynchronous-Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,40 @@
 function solve() {
+    const busStopElement = document.getElementsByClassName('info')[0];
+    const departButton = document.getElementById('depart');
+    const arriveButton = document.getElementById('arrive');
+    let stop = 'depot';
 
     function depart() {
-        console.log('Depart TODO...');
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop}`;
+        fetch(url).then(response => {
+            if (response.status != 200) {
+                throw 'Error';
+            }
+            return response.json();
+        }).then(data => {
+            busStopElement.textContent = `Next stop ${data.name}`;
+            departButton.disabled = true;
+            arriveButton.disabled = false;
+        }).catch(error => {
+            busStopElement.textContent = error;
+        })
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop}`;
+        fetch(url).then(response => {
+            if (response.status != 200) {
+                throw 'Error';
+            }
+            return response.json();
+        }).then(data => {
+            busStopElement.textContent = `Arriving at ${data.name}`;
+            departButton.disabled = false;
+            arriveButton.disabled = true;
+            stop = data.next;
+        }).catch(error => {
+            busStopElement.textContent = error;
+        })
     }
 
     return {
