355f946ea1d414328dba1097bff3119aaeb39f81
Kiril Yanev <kl_yanev@abv.bg>
2023-03-10 23:37:18 +0200

    08-Exercise-Remote-Data-and-Authentication
    05.Fisher-Game
    Implement User Authentication

--- 08-Exercise-Remote-Data-and-Authentication/05.Fisher-Game/src/login.js
+++ 08-Exercise-Remote-Data-and-Authentication/05.Fisher-Game/src/login.js
@@ -1,1 +1,55 @@
-console.log('TODO:// Implement Login functionality');
+document.querySelectorAll("a").forEach(el => el.classList.remove("active"));
+document.getElementById("login").classList.add("active");
+document.getElementById('user').style.display = 'none';
+
+function login() {
+    const button = document.querySelector('button');
+    
+    button.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        const url = 'http://localhost:3030/users/login';
+
+        const errNotification = document.querySelector('.notification');
+
+        const loginFormElement = document.getElementById('login-form');
+        const data = new FormData(loginFormElement);
+
+        const [email,password] = [...data.values()];
+
+        // Email and pass validation
+        if(email == '' || password == '') {
+            return alert('Invalid email or password!');
+        }
+
+        const body = {
+            email: email,
+            password: password
+        }
+
+        const options = {
+            method: 'POST',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(body)
+        }
+
+        fetch(url,options).then(response => {
+            if(response.status != 200) {
+                throw new Error();
+            }
+            return response.json();
+        }).then(data => {
+            sessionStorage.setItem('email', data.email);
+            sessionStorage.setItem('accessToken',data.accessToken);
+
+            loginFormElement.reset();
+            window.location.href="./index.html";
+        }).catch(err => {
+            errNotification.textContent = err;
+            setTimeout(() => {
+            errNotification.textContent = '';
+            },1000);
+        })
+    })
+}
+
+login();
--- 08-Exercise-Remote-Data-and-Authentication/05.Fisher-Game/src/register.js
+++ 08-Exercise-Remote-Data-and-Authentication/05.Fisher-Game/src/register.js
@@ -1,1 +1,54 @@
-console.log('TODO:// Implement Register functionality');
+document.querySelectorAll("a").forEach(el => el.classList.remove("active"));
+document.getElementById("register").classList.add("active");
+document.getElementById('user').style.display = 'none';
+
+function register() {
+    const buttonElement = document.querySelector('button');
+    const url = 'http://localhost:3030/users/register';
+
+    buttonElement.addEventListener('click',(ev) => {
+        ev.preventDefault();
+        const errNotification = document.querySelector('.notification');
+        const registerFormElement = document.getElementById('register-form');
+        const data = new FormData(registerFormElement);
+
+        const [email,password,rePass] = [...data.values()];
+
+        if(email == '') {
+            return errNotification.textContent = 'Error: Enter correct email!';
+        } else if (password == '' || password != rePass) {
+            return errNotification.textContent = "Error: passwords don't match!";
+        }
+        const registerData = {
+            email: email,
+            password:password
+        }
+
+        const options = {
+            method: 'post',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(registerData)
+        }
+
+        fetch(url,options).then(response => {
+            if (response.status == 409) {
+                errNotification.textContent = 'User exists';
+                throw new Error('User exists')
+            }
+            return response.json();
+        }).then(data => {
+            sessionStorage.setItem("email",email);
+            sessionStorage.setItem("password", password);
+            
+            registerFormElement.reset();
+            window.location.href="./index.html";
+        }).catch(err => {
+            errNotification.textContent = err;
+            setTimeout(()=>{
+                errNotification.textContent = '';
+            },1000);
+        })
+    })
+}
+
+register();
