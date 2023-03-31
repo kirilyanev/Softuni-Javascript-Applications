ffba8a781254f14975b0ab5522bdee9a8c23ebc1
Kiril Yanev <kl_yanev@abv.bg>
2023-03-31 15:39:52 +0300

    19-Exam-preparation
    Clear-Career

--- 19-Exam-preparation/Clear-Career/src/app.js
+++ 19-Exam-preparation/Clear-Career/src/app.js
@@ -1,18 +1,29 @@
 // TODO import libraries and change current imports to correct paths
-// import page from 'node_modules/page/page.mjs -> update path';
-// import { render } from 'node_modules/lit-html/lit-html.js -> update path';
+import page from '../node_modules/page/page.mjs';
+import { render } from '../node_modules/lit-html/lit-html.js';
+import { layoutTemplate } from './views/layout.js';
 import { logout } from "./data/auth.js";
 import { getUserData } from "./data/util.js";
 import { homePage } from "./views/home.js";
 import { loginPage } from "./views/login.js";
 import { registerPage } from "./views/register.js";
+import { catalogPage } from './views/catalog.js';
+import { detailsPage } from './views/details.js';
+import { createPage } from './views/create.js';
+import { editPage } from './views/edit.js';
 
-// TODO change render root depending on project HTML structure
-const root = document.body;
+import * as api from './data/applications.js';
+window.api = api;
+
+const root = document.getElementById('wrapper');
 
 page(decorateContext);
 page('index.html', '/');
 page('/', homePage);
+page('/catalog', catalogPage);
+page('/catalog/:id', detailsPage);
+page('/catalog/:id/edit', editPage);
+page('/create', createPage);
 page('/login', loginPage);
 page('/register', registerPage);
 page('/logout', logoutAction);
